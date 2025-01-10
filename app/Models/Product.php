c65c8f9f1bf39a54f73eb43eb012aa4571999804
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-10 23:17:13 +0800

    Product models updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use App\Models\Photo;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        "title",
+        "slug",
+        "description",
+        "price",
+        "medium",
+        "surface",
+        "year_of_creation",
+        "stock",
+        "size"
+    ];
+
+    public function photos() 
+    {
+        $this->hasMany(Photo::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
