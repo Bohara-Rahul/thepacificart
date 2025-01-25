dd0501b121e47ff6293e23ff2f5dc88b7a85c9f2
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-25 16:00:11 +0800

    Changed hero description

--- resources/views/front/components/hero-section.blade.php
+++ resources/views/front/components/hero-section.blade.php
@@ -1,7 +1,7 @@
 <section class="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center" style="margin-top: 160px;">
   <aside class="flex flex-col justify-center text-2xl">
     <h1 class="max-w-xl font-bold text-4xl tracking-tight sm:text-6xl">Art that travels, Elegance that stays</h1>
-    <p class="mt-8 max-w-xl text-lg leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>Voluptas vero totam exercitationem ex nesciunt debitis earum, illo explicabo beatae eum. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem molestias expedita est laboriosam sequi odit nostrum nulla quod at officia.</p>
+    <p class="mt-8 max-w-xl text-lg leading-8">Welcome to a world where art transcends borders. We specialize in curating exquisite oil paintings that bring timeless elegance to your space, no matter where you are.<br/>Our carefully crafted collections are designed to inspire, elevate, and create lasting impressions. Explore our gallery and find the perfect masterpiece to make your home or office truly unforgettable</p>
     <a class="btn mt-8 w-[150px] text-center">Learn More</a>
   </aside>
     <img 
