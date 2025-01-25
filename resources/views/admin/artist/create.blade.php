84046846318cd3c795c655fcca0aedfbcab891a7
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-25 17:09:25 +0800

    Error mmessage in form

--- resources/views/admin/artist/create.blade.php
+++ resources/views/admin/artist/create.blade.php
@@ -45,5 +45,16 @@
     </div>
     <button type="submit">Add</button>
   </form>
+
+  <!-- validation errors -->
+  @if ($errors->any())
+    <ul class="max-w-2xl px-4 py-2 bg-red-100">
+      @foreach ($errors->all() as $error)
+        <li class="my-2 text-red-500">
+          {{ $error }}
+        </li>
+      @endforeach
+    </ul>
+  @endif
 </section>
 @endsection
--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -17,5 +17,16 @@
     </div>
     <button type="submit">Create</button>
   </form>
+  
+  <!-- validation errors -->
+  @if ($errors->any())
+    <ul class="max-w-2xl px-4 py-2 bg-red-100">
+      @foreach ($errors->all() as $error)
+        <li class="my-2 text-red-500">
+          {{ $error }}
+        </li>
+      @endforeach
+    </ul>
+  @endif
 </section>
 @endsection
