1718601b009644cd5e57b5425ce448b5e279a9c8
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-26 00:40:22 +0800

    Category Detail Page Added

--- resources/views/front/category_detail.blade.php
+++ resources/views/front/category_detail.blade.php
@@ -1,11 +1,14 @@
-<section>
-  <h3 class="section-heading">
-    {{ $category->title }}
-  </h3>
-  @foreach ($products as $product)
-     <article>
-        <h4>{{ $product->title }}</h4>
-        <p>{{ $product->price }}</p>
-     </article> 
-  @endforeach
-</section>
+@extends('layouts.front')
+@section('main_content')
+  <section>
+    <h3 class="section-heading">
+      {{ $category->title }}
+    </h3>
+    @foreach ($products as $product)
+      <article>
+          <h4>{{ $product->title }}</h4>
+          <p>{{ $product->price }}</p>
+      </article> 
+    @endforeach
+  </section>
+@endsection
