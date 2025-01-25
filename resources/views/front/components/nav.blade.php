d78de3eb1a5e79ef0139530d919baf3fe436e498
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-25 10:30:30 +0800

    Added transparent logo

--- resources/views/front/components/nav.blade.php
+++ resources/views/front/components/nav.blade.php
@@ -13,7 +13,7 @@
   </ul>
   <div>
     <a href="/">
-      <img src="{{ asset('logo.png') }}" alt="logo" style="height: 140px; object-fit: cover;" />
+      <img src="{{ asset('logo_transparent.png') }}" alt="logo" style="height: 140px; object-fit: cover;" />
     </a>
   </div>
   <ul class="flex justify-between gap-5 items-center text-xl">
