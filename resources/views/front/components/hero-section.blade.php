1d3450585cc09a317ecab5d388aa31a08f17b5e7
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-25 15:11:10 +0800

    Slogan changed

--- resources/views/front/components/hero-section.blade.php
+++ resources/views/front/components/hero-section.blade.php
@@ -1,6 +1,6 @@
 <section class="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center" style="margin-top: 160px;">
   <aside class="flex flex-col justify-center text-2xl">
-    <h1 class="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">Slogan goes here</h1>
+    <h1 class="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">Art That Travels, Elegance That Stays</h1>
     <p class="mt-8 max-w-xl text-lg leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>Voluptas vero totam exercitationem ex nesciunt debitis earum, illo explicabo beatae eum. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem molestias expedita est laboriosam sequi odit nostrum nulla quod at officia.</p>
     <a class="btn mt-8 w-[150px] text-center">Learn More</a>
   </aside>
