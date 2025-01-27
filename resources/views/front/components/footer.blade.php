51fc58793a427d8c93e797f3f689eaa1840f66dc
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-27 13:33:25 +0800

    Created footer as a different component

---
+++ resources/views/front/components/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer p-5">
+  <div class="container grid grid-cols-3">
+    <ul class="flex flex-col gap-2">
+    <p class="text-xl">Contact Us</p>
+      <li><i class="fa-solid fa-phone mr-2"></i> +61 450 213 015</li>
+      <li><i class="fa-solid fa-envelope mr-2"></i> info@thepacificart.com.au</li>
+    </ul>
+        
+        <ul class="flex flex-col space-x-2">
+        <p class="text-xl">Social Media</p>
+        <div class="flex gap-5">
+          <a href="https://www.facebook.com/profile.php?id=61571620204348" target="_blank">
+          <li>
+            <i class="fa-brands fa-facebook text-lg"></i>
+          </li>
+          </a>
+        
+        <li href="#">
+          <i class="fa-brands fa-instagram text-lg"></i>
+        </li>
+        </div>
+        
+        </ul>
+        
+      <ul>
+        <li>Terms and Conditions</li>
+        <li>Return Policy</li>
+      </ul> 
+    </div>
+
+ <p class="text-center">
+  All rights reserved with &copy; {{ date('Y') }}
+ </p>
+  
+</footer>
