e381d310341b702aec8b58ed7aa4cce88173424f
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-01-27 11:34:33 +0800

    Navbar fixed on scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,17 @@
 import './bootstrap';
 
-const header = document.getElementById('header');
-const heroSection = ddocument.getElementById('hero-section');
+const navContainer = document.getElementById('nav-container');
+const navBar = document.getElementById('nav-bar');
 
-// Callback function
-const callback = (entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      header.style.background = 'red';
-      // entry.heroSection.classList.add("observed"); // Add class when in view
-      console.log("Element is in view!");
-    } 
-  });
-};
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.pageYOffset;
+  const navContainerHeight = navContainer.getBoundingClientRect().height;
 
-  // Observer options
-  const options = {
-    root: null, // Default is the viewport
-    threshold: 0.5 // 50% of the element must be visible to trigger
-  };
-
-  // Create the observer
-  const observer = new IntersectionObserver(callback, options);
-
-  // Observe the target element
-  observer.observe(heroSection);
+  if(scrollHeight > navContainerHeight) {
+    navContainer.classList.add('fixed');
+    navBar.style.color = "#13292a";
+  } else {    
+    navContainer.classList.remove('fixed');
+  }
+});
 
